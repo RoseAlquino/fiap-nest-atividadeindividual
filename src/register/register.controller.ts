8f62d0906fe380e926edb45ae8070e7b4e9a4b51
Rose Alquino <r.alquino.dos.santos@avanade.com>
2022-10-28 17:07:28 -0300

    Deploy

--- src/register/register.controller.ts
+++ src/register/register.controller.ts
@@ -1,18 +1,32 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get, Param, ParseUUIDPipe, Patch, } from '@nestjs/common';
 import { RegisterService } from './register.service';
+import { CreateRegisterDTO } from './dto/createRegister.dto';
+import { UpdateRegisterDTO } from './dto/updateRegister.dto';
+
 
 @Controller('register')
 export class RegisterController {
 	constructor(private readonly registerService: RegisterService) {}
 
-	@Post('login')
-	async registerLogin(@Body() req) {
-		const {name, email, password, site, phone} = req;
-		console.log('Name:', name);
-		console.log('Email:', email);
-		console.log('Senha:', password);
-		console.log('Site:', site);
-		console.log('Phone:', phone);
-		return `Cliente ${name} cadastrado com sucesso!`;
-}
-}
+
+	@Post()
+	create(@Body() req: CreateRegisterDTO) {
+		return this.registerService.create();
+	}
+
+	@Get()
+	findAll() {
+		return this.registerService.findAll();
+	}
+
+	@Get(':id')
+	findOne(@Param('id', ParseUUIDPipe) id: number ) {
+		return this.registerService.findOne(id);
+	}
+
+	@Patch(':id')
+	update(@Param('id', ParseUUIDPipe) id: number, @Body() req: UpdateRegisterDTO) {
+		return this.registerService.update(id, req);
+	}}
+
+
--- src/register/register.service.ts
+++ src/register/register.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { UpdateRegisterDTO } from './dto/updateRegister.dto';
 
 @Injectable()
 export class RegisterService {
-	
 
-	async registerLogin(name: string, email: string, password: string, site: string, phone: string) {
-		console.log('No Service - Name:', name);
-		console.log('No Service - Email:', email);
-		console.log('No Service - Senha:', password);
-		console.log('No Service - Site:', site);
-		console.log('No Service - Phone:', phone);
-		return {name, email, password, site, phone};
+	async create(): Promise<String> {
+		return 'Usuário criado com sucesso!';
+	}
+
+	async findAll(): Promise<String> {
+		return 'Lista de usuários!';
+	}
+
+	async findOne(id: number): Promise<String> {
+		return `Usuário ${id} encontrado!`;
+	}
+
+	async update(id: number, req: UpdateRegisterDTO): Promise<String> {
+		return `Usuário ${id} atualizado com sucesso!`;
 	}
 }
